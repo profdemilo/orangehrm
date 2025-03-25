2992c08b2a7f959ca96d787512d605af130934bf
Iverson <takwiiverson1@gmail.com>
2025-03-26 00:31:28 +0100

    admin test

--- cypress/e2e/orangehrm/pim.cy.js
+++ cypress/e2e/orangehrm/pim.cy.js
@@ -17,44 +17,44 @@ describe('Orange HRM - PIM Test', function() {
   
     })
 
-   /*cy.get(':nth-child(2) > .oxd-main-menu-item').click()
-   cy.get('.--parent > .oxd-topbar-body-nav-tab-item').click()
-   cy.get(':nth-child(1) > .oxd-topbar-body-nav-tab-link').click() */
+    /*cy.get(':nth-child(2) > .oxd-main-menu-item').click()
+    cy.get('.--parent > .oxd-topbar-body-nav-tab-item').click()
+    cy.get(':nth-child(1) > .oxd-topbar-body-nav-tab-link').click() */
     
   
   
     it('Configuration', () => {
-        //Optional Fields
+    //Optional Fields
         
-            cy.get('.--parent > .oxd-topbar-body-nav-tab-item').click()
-            cy.get(':nth-child(1) > .oxd-topbar-body-nav-tab-link').should('contain.text','Optional Fields').click()
-            cy.url().should('contain','/configurePim')
-            cy.get('.oxd-topbar-header-breadcrumb').should('contain', 'PIM').and('contain', 'Configuration')    
-        //Custom Fields 
+    cy.get('.--parent > .oxd-topbar-body-nav-tab-item').click()
+    cy.get(':nth-child(1) > .oxd-topbar-body-nav-tab-link').should('contain.text','Optional Fields').click()
+    cy.url().should('contain','/configurePim')
+    cy.get('.oxd-topbar-header-breadcrumb').should('contain', 'PIM').and('contain', 'Configuration')    
+    //Custom Fields 
         
-            cy.get('.--parent > .oxd-topbar-body-nav-tab-item').click()
-            cy.get(':nth-child(2) > .oxd-topbar-body-nav-tab-link').should('contain.text','Custom Fields').click()
-            cy.url().should('contain','/listCustomFields')
-            cy.get('.oxd-topbar-header-breadcrumb').should('contain', 'PIM').and('contain', 'Configuration')    
-        //Data Import
+    cy.get('.--parent > .oxd-topbar-body-nav-tab-item').click()
+    cy.get(':nth-child(2) > .oxd-topbar-body-nav-tab-link').should('contain.text','Custom Fields').click()
+    cy.url().should('contain','/listCustomFields')
+    cy.get('.oxd-topbar-header-breadcrumb').should('contain', 'PIM').and('contain', 'Configuration')    
+    //Data Import
         
-            cy.get('.--parent > .oxd-topbar-body-nav-tab-item').click()
-            cy.get(':nth-child(3) > .oxd-topbar-body-nav-tab-link').should('contain.text','Data Import').click()
-            cy.url().should('contain','/pimCsvImport')
-            cy.get('.oxd-topbar-header-breadcrumb').should('contain', 'PIM').and('contain', 'Configuration')  
-        //Reporting Methods
+    cy.get('.--parent > .oxd-topbar-body-nav-tab-item').click()
+    cy.get(':nth-child(3) > .oxd-topbar-body-nav-tab-link').should('contain.text','Data Import').click()
+    cy.url().should('contain','/pimCsvImport')
+    cy.get('.oxd-topbar-header-breadcrumb').should('contain', 'PIM').and('contain', 'Configuration')  
+    //Reporting Methods
            
-            cy.get('.--parent > .oxd-topbar-body-nav-tab-item').click()
-            cy.get(':nth-child(4) > .oxd-topbar-body-nav-tab-link').should('contain.text','Reporting Methods').click()
-            cy.url().should('contain','/viewReportingMethods')
-            cy.get('.oxd-topbar-header-breadcrumb').should('contain', 'PIM').and('contain', 'Configuration')    
-        //Termination Reasons
+    cy.get('.--parent > .oxd-topbar-body-nav-tab-item').click()
+    cy.get(':nth-child(4) > .oxd-topbar-body-nav-tab-link').should('contain.text','Reporting Methods').click()
+    cy.url().should('contain','/viewReportingMethods')
+    cy.get('.oxd-topbar-header-breadcrumb').should('contain', 'PIM').and('contain', 'Configuration')    
+    //Termination Reasons
         
-        cy.get('.--parent > .oxd-topbar-body-nav-tab-item').click()
-        cy.get(':nth-child(5) > .oxd-topbar-body-nav-tab-link').should('contain.text','Termination Reasons').click()
-        cy.url().should('contain','/viewTerminationReasons')
-        cy.get('.oxd-topbar-header-breadcrumb').should('contain', 'PIM').and('contain', 'Configuration')    
-        })
+    cy.get('.--parent > .oxd-topbar-body-nav-tab-item').click()
+    cy.get(':nth-child(5) > .oxd-topbar-body-nav-tab-link').should('contain.text','Termination Reasons').click()
+    cy.url().should('contain','/viewTerminationReasons')
+    cy.get('.oxd-topbar-header-breadcrumb').should('contain', 'PIM').and('contain', 'Configuration')    
+    })
 
     it('Employee-List', () => {
     cy.get('.oxd-topbar-body-nav-tab-item').contains('Employee List').click()
@@ -83,6 +83,4 @@ describe('Orange HRM - PIM Test', function() {
     cy.get('button').contains('Add').click()
     cy.url().should('contain', 'https://opensource-demo.orangehrmlive.com/web/index.php/pim/definePredefinedReport')
     })
-  
-
-})
+    })
