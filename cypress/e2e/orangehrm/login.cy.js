a29fc9b02f74011c09a66d6ce81d5478e57e9a24
Iverson <takwiiverson1@gmail.com>
2025-03-26 16:20:07 +0100

    complete

--- cypress/e2e/orangehrm/login.cy.js
+++ cypress/e2e/orangehrm/login.cy.js
@@ -8,25 +8,23 @@ describe('Orange HRM - Login Test', () => {
     let password = 'admin123'
     const loginPage2 = new loginPageObjects()
 
-    beforeEach(() => {
+beforeEach(() => {
 
-        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    })
+cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+})
 
-    it('Negative Testing', () => {
-        loginPage2.enterUsername(good_username)
-        loginPage2.enterPassword(bad_password)
-        loginPage2.loginButton().click()
-        loginPage2.loginErrorMessage('Invalid credentials')
-        cy.url().should('contain', '/login')
-    })
+it('Negative Testing', () => {
+loginPage2.enterUsername(good_username)
+loginPage2.enterPassword(bad_password)
+loginPage2.loginButton().click()
+loginPage2.loginErrorMessage('Invalid credentials')
+cy.url().should('contain', '/login')
+})
 
-    it('Positive Testing', () => {
-        loginPage2.enterUsername(good_username)
-        loginPage2.enterPassword(password)
-        loginPage2.loginButton().click()
-        cy.url().should('contain', '/dashboard')
-    })
-
-
+it('Positive Testing', () => {
+loginPage2.enterUsername(good_username)
+loginPage2.enterPassword(password)
+loginPage2.loginButton().click()
+cy.url().should('contain', '/dashboard')
+})
 })
